9d220ea13b218d97582a70c62de741c25d5c9ff0
Mohamad Eldhemy <imdhemy@gmail.com>
2022-04-18 15:31:34 +0300

    add students endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,8 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -15,16 +10,5 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @GetMapping
-    public List<Student> hello() {
-        return List.of(
-                (new Student())
-                        .setId(1L)
-                        .setName("Mariam")
-                        .setEmail("mariam@gmail.com")
-                        .setDob(LocalDate.of(2000, 9, 9))
-        );
     }
 }
